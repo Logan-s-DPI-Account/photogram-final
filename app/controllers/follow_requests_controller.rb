c6492d8af9eb428b8f9798ad1a20b71128a0d81f
Logan CoBell <lcobell@cps.edu>
2022-11-05 02:03:02 +0000

    follow request are now created when you click follow from the users index page

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -22,6 +22,8 @@ class FollowRequestsController < ApplicationController
     the_follow_request.sender_id = params.fetch("query_sender_id")
     the_follow_request.recipient_id = params.fetch("query_recipient_id")
     # the_follow_request.status = params.fetch("query_status")
+    the_follow_request.status = "pending"
+
    
     if the_follow_request.valid?
       the_follow_request.save
